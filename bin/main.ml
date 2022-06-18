016db06f173419151037de18858b87b48b2a6d42
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-06-18 22:30:46 +0200

    Implement tests

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,12 @@
 module GV = Graph.Graphviz.Dot (Cardinal)
 module H = Hamilton.Bitfield (Cardinal) (Cardinal.Mark)
+open Cardinal
 
-let read_lines () =
+let input_lines ch =
   let rec f acc =
     match
       try
-        let l = read_line () in
+        let l = input_line ch in
         Some l
       with End_of_file ->
         None
@@ -15,26 +16,44 @@ let read_lines () =
   in
   f []
 
+
 let () =
-  let t = read_lines () |> Cardinal.of_rows in
   match Sys.argv with
-  | [| _ |] ->
-    GV.output_graph stdout t;
-    print_newline ()
-  | [| _; "-r" |] ->
-    let open Cardinal in
-    begin match H.decompose is_start t with
-    | None -> print_endline "not found"
-    | Some paths ->
-      if valid_solution t paths then
-        List.iter (fun p ->
-          Printf.printf "path:";
-          List.iter (fun x -> Printf.printf " %s" (vertex_name x)) p;
-          Printf.printf "\n"
-        ) paths
-      else
-        print_endline "invalid solution"
-    end
-  | _ ->
-    Printf.eprintf "usage: %s [-r]\n" Sys.argv.(0);
+  | [| _; "-g" |] ->
+    input_lines stdin |> of_rows |> GV.output_graph stdout;
+    print_newline ();
+    exit 0
+  | [| _; "-h" |] | [| _; "--help" |] ->
+    Printf.printf "usage: %s -g | puzzle...\n" Sys.argv.(0);
     exit 1
+  | _ ->
+    let ct = Array.length Sys.argv - 1 in
+    let ok = ref true in
+    Printf.printf "1..%d\n" ct;
+    for i = 1 to ct do
+      let inp = Sys.argv.(i) in
+      let report good s =
+        Printf.printf "%sok - %s: %s\n" (if good then "" else "not ") inp s;
+        ok := !ok && good
+      in
+      try
+        let ch =
+          match inp with
+          | "-" -> stdin
+          | fn -> open_in fn
+        in
+        let t = input_lines ch |> of_rows in
+        begin match H.decompose is_start t with
+        | None -> report false "no solution found"
+        | Some paths ->
+          if valid_solution t paths then
+            List.map (fun p -> List.map vertex_name p |> String.concat " ") paths |>
+            String.concat ", " |>
+            report true
+          else
+            report false "invalid solution"
+        end
+      with e ->
+        report false (Printexc.to_string e)
+    done;
+    if not !ok then exit 2
